25043f1158e1fffa9e57353415aa69a2d73cfd14
Dimitris Petrou <petroudimitris@outlook.com>
2024-08-12 19:36:42 +0300

    Adjustment to attach cluster info the experiment info

--- src/experiments/get_experiments.php
+++ src/experiments/get_experiments.php
@@ -2,6 +2,21 @@
 
 include_once '../minio.php'; // Adjust the path if necessary
 
+// Function to get clusters
+function getClusters($s3) {
+    try {
+        $result = getObject($s3, 'clusters.json');
+        $clusters = json_decode($result['Body'], true);
+        if (!is_array($clusters)) {
+            $clusters = [];
+        }
+    } catch (Exception $e) {
+        $clusters = [];
+    }
+    return $clusters;
+}
+
+// Function to get experiments
 function getExperiments($s3) {
     try {
         $result = getObject($s3, 'experiments.json');
@@ -15,9 +30,41 @@ function getExperiments($s3) {
     return $experiments;
 }
 
-if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-    $experiments = getExperiments($s3);
-    echo json_encode($experiments);
+// Function to get cluster info by UID
+function getClusterByUid($clusters, $uid) {
+    foreach ($clusters as $cluster) {
+        if ($cluster['uid'] === $uid) {
+            return [
+                'name' => $cluster['name'],
+                'ip' => $cluster['apiserviceaddr']
+            ];
+        }
+    }
+    return null;
 }
 
+// Handle GET request
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    $experiments = getExperiments($s3);
+    $clusters = getClusters($s3);
+    
+    // Attach cluster info to each experiment
+    foreach ($experiments as &$experiment) {
+        if (isset($experiment['sde_cluster'])) {
+            $clusterInfo = getClusterByUid($clusters, $experiment['sde_cluster']);
+            if ($clusterInfo) {
+                $experiment['cluster_name'] = $clusterInfo['name'];
+                $experiment['cluster_ip'] = $clusterInfo['ip'];
+            } else {
+                $experiment['cluster_name'] = null;
+                $experiment['cluster_ip'] = null;
+            }
+        } else {
+            $experiment['cluster_name'] = null;
+            $experiment['cluster_ip'] = null;
+        }
+    }
+
+    echo json_encode($experiments);
+}
 ?>
